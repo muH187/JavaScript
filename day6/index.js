4062f71a7ef3cf4545be8ac00caaeb1d20af1394
muH187 <68023597+muH187@users.noreply.github.com>
2023-12-01 16:01:58 +0500

    I have learned strings and it's functions

---
+++ day6/index.js
@@ -0,0 +1,33 @@
+console.log("This is strings tutorial")
+
+let a = "Ali"
+const b = "I love Coding"
+
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+
+console.log(a.length)
+
+console.log(b.length)
+console.log(b[5])
+
+var real_name = "Muhammad Ali"
+var friend = "Muhammad Mirza"
+
+console.log("his name is " + real_name + " and his friend name is " + friend)
+console.log(`his name is ${real_name} and his friend name is ${friend}`)
+
+let c = "pakistan"
+console.log(c.toUpperCase())
+console.log(c.length)
+console.log(c.slice(0, 8))
+console.log(c.slice(5))
+console.log(c.replace("pak", "PAK"))
+console.log(c.replace("n", "Nn"))
+console.log(c.concat(b))
+
+let country_name = "Pakistan"
+let your_verb = "I love "
+
+console.log(your_verb.concat(country_name))
