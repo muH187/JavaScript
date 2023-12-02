4129c7d71aadaf553b93b539e90e0954113e5cbf
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-02 11:35:34 +0500

    arrays and its methods

---
+++ day7/script.js
@@ -0,0 +1,32 @@
+let arr = [1, 2, 3, 4, 5, 7, 9]
+arr[0] = 100 
+arr[7] = 200
+
+// console.log(arr)
+// console.log(arr.length)
+// console.log(arr[6])
+
+console.log(arr.toString())
+// console.log(arr)
+console.log(arr.join(" and "))
+
+let a1 = [1, 2, 3, 4]
+let a2 = [5, 6, 7, 8]
+let a3 = [9, 10, 11, 12]
+
+a4 = a1.concat(a2, a3)
+console.log(a4)
+
+let array2 = ["Muhammad", "Ali", "Muhammad", "Mirza", 23, 43, 10, 29, "Wali", "Basit", "Zohaib", "Umer", "Yasir", 234, 2, 19]
+
+// push  unshift ka bhai hai 
+// or
+// pop   shift ka bhai hai
+
+console.log(array2)
+console.log(array2.length)
+console.log(array2.push("Pakistan"))
+console.log(array2.pop())
+console.log(array2)
+
+
