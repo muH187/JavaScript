1b6b7a4423d5c9c19e29d89144b8f6eecb30d888
Muhammad Ali <proqualitythings100@gmail.com>
2023-12-02 15:24:47 +0500

    loops with array

---
+++ day7/loop.js
@@ -0,0 +1,33 @@
+let a = [1, 2, 35, 25, 12, 65]
+
+// for (let index = 0; index < a.length; index++) {
+//     const element = a[index];
+//     console.log(element)
+    
+// }
+
+// a.forEach((value, index, arr)=>{
+//     console.log(value, index, arr)
+// })
+
+
+let obj = {
+    a: 1,
+    b: 2,
+    c: 3
+}
+
+// For-in
+
+// for (const key in obj) {
+//     if (obj.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//         console.log(key, element)
+//     }
+// }
+
+// For-of
+
+// for (const value of a) {
+//     console.log(value)
+// }
---
+++ day7/mfr.js
@@ -0,0 +1,15 @@
+let arr = [1, 2, 3, 4, 20, 23, 10, 5, 8, 12]
+
+// let newArr = []
+
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     newArr.push(element**2)
+    
+// }
+
+let newArr = arr.map((e)=>{
+    return e**2
+})
+
+console.log(newArr)
